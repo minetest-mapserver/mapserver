5ebc282bf96950789e1ccaf9f6721c4856fdfc1a
NatureFreshMilk <naturefreshmilk@github.com>
2019-01-11 09:07:31 +0100

    accessor cache

--- go.mod
+++ go.mod
@@ -2,5 +2,6 @@ module mapserver
 
 require (
 	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/sirupsen/logrus v1.3.0
 )
--- mapblockaccessor/mapblockaccessor.go
+++ mapblockaccessor/mapblockaccessor.go
@@ -4,30 +4,54 @@ import (
 	"mapserver/coords"
 	"mapserver/db"
 	"mapserver/mapblockparser"
+	"github.com/patrickmn/go-cache"
+	"time"
+	"fmt"
 )
 
 type MapBlockAccessor struct {
 	accessor db.DBAccessor
+	c *cache.Cache
+}
+
+func getKey(pos coords.MapBlockCoords) string {
+	return fmt.Sprintf("Coord %d/%d/%d", pos.X, pos.Y, pos.Z)
 }
 
 func NewMapBlockAccessor(accessor db.DBAccessor) *MapBlockAccessor {
-	return &MapBlockAccessor{accessor: accessor}
+	c := cache.New(5*time.Minute, 10*time.Minute)
+
+	return &MapBlockAccessor{accessor: accessor, c: c}
 }
 
 func (a *MapBlockAccessor) Update(pos coords.MapBlockCoords, mb *mapblockparser.MapBlock) {
-	//TODO: cache
+	key := getKey(pos)
+	a.c.Set(key, mb, cache.DefaultExpiration)
 }
 
 func (a *MapBlockAccessor) GetMapBlock(pos coords.MapBlockCoords) (*mapblockparser.MapBlock, error) {
+	key := getKey(pos)
+
+	cachedblock, found := a.c.Get(key)
+	if found {
+		return cachedblock.(*mapblockparser.MapBlock), nil
+	}
+
 	block, err := a.accessor.GetBlock(pos)
 	if err != nil {
 		return nil, err
+	}
+
+	if block == nil {
+		return nil, nil
 	}
 
 	mapblock, err := mapblockparser.Parse(block.Data)
 	if err != nil {
 		return nil, err
 	}
+
+	a.c.Set(key, mapblock, cache.DefaultExpiration)
 
 	return mapblock, nil
 }
