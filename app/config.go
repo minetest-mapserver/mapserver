4b5b63fe9eaa0aaad67c4dc872a659be75a988be
NatureFreshMilk <naturefreshmilk@github.com>
2019-01-18 10:11:40 +0100

    webserver stub

--- app/config.go
+++ app/config.go
@@ -9,12 +9,14 @@ import (
 type Config struct {
 	Port                   int  `json:"port"`
 	EnableInitialRendering bool `json:"enableinitialrendering"`
+	Webdev                 bool `json:"webdev"`
 }
 
 func ParseConfig(filename string) (*Config, error) {
 	cfg := Config{
 		Port:                   8080,
 		EnableInitialRendering: true,
+		Webdev:                 false,
 	}
 
 	info, err := os.Stat(filename)
--- web/serve.go
+++ web/serve.go
@@ -1,27 +1,31 @@
 package web
 
 import (
-  "github.com/sirupsen/logrus"
-  "mapserver/app"
-  "net/http"
-  "strconv"
-  "fmt"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"mapserver/app"
+	"mapserver/vfs"
+	"net/http"
+	"strconv"
 )
 
-func Serve(ctx *app.App){
-  fields := logrus.Fields{
-    "port":           ctx.Config.Port,
-  }
-  logrus.WithFields(fields).Info("Starting http server")
+func Serve(ctx *app.App) {
+	fields := logrus.Fields{
+		"port":   ctx.Config.Port,
+		"webdev": ctx.Config.Webdev,
+	}
+	logrus.WithFields(fields).Info("Starting http server")
 
-  mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-  mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Welcome to my website!")
-  })
+	mux.Handle("/", http.FileServer(vfs.FS(ctx.Config.Webdev)))
 
-  err := http.ListenAndServe(":" + strconv.Itoa(ctx.Config.Port), mux)
-  if err != nil {
-    panic(err)
-  }
+	mux.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Api endpoint")
+	})
+
+	err := http.ListenAndServe(":"+strconv.Itoa(ctx.Config.Port), mux)
+	if err != nil {
+		panic(err)
+	}
 }
