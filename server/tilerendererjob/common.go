bf764b1a34f2ecdbbd991a801c64cacd26fca9be
Thomas Rudin <thomas@rudin.io>
2019-01-27 18:34:04 +0100

    fix tile render hickups

--- server/tilerendererjob/common.go
+++ server/tilerendererjob/common.go
@@ -39,37 +39,6 @@ func renderMapblocks(ctx *app.App, jobs chan *coords.TileCoords, mblist []*mapbl
 				"Zoom":    tc.Zoom,
 				"LayerId": tc.LayerId,
 			}
-			logrus.WithFields(fields).Debug("Tile removing (z12-1)")
-
-			//remove tile
-			ctx.Objectdb.RemoveTile(tc)
-		}
-	}
-
-	tileRenderedMap = make(map[string]bool)
-
-	for i := 12; i >= 1; i-- {
-		for _, mb := range mblist {
-			//13
-			tc := coords.GetTileCoordsFromMapBlock(mb.Pos, ctx.Config.Layers)
-
-			//12-1
-			tc = tc.ZoomOut(13 - i)
-
-			key := getTileKey(tc)
-
-			if tileRenderedMap[key] {
-				continue
-			}
-
-			tileRenderedMap[key] = true
-
-			fields := logrus.Fields{
-				"X":       tc.X,
-				"Y":       tc.Y,
-				"Zoom":    tc.Zoom,
-				"LayerId": tc.LayerId,
-			}
 			logrus.WithFields(fields).Debug("Dispatching tile rendering (z12-1)")
 
 			tilecount++
--- server/tilerendererjob/worker.go
+++ server/tilerendererjob/worker.go
@@ -7,6 +7,10 @@ import (
 
 func worker(ctx *app.App, coords <-chan *coords.TileCoords) {
 	for tc := range coords {
+		//remove tile
+		ctx.Objectdb.RemoveTile(tc)
+
+		//render tile
 		_, err := ctx.Tilerenderer.Render(tc, 5)
 		if err != nil {
 			panic(err)
