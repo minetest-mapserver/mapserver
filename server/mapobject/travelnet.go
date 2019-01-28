c6adc74f2d283bf79ff5f8195a6d34f74b655523
NatureFreshMilk <naturefreshmilk@github.com>
2019-01-28 14:37:58 +0100

    empty travelnet

--- server/mapobject/travelnet.go
+++ server/mapobject/travelnet.go
@@ -10,11 +10,6 @@ type TravelnetBlock struct{}
 func (this *TravelnetBlock) onMapObject(x, y, z int, block *mapblockparser.MapBlock) *mapobjectdb.MapObject {
 	md := block.Metadata.GetMetadata(x, y, z)
 
-	if md["station_name"] == "" || md["owner"] == "" {
-		//station not set up
-		return nil
-	}
-
 	o := mapobjectdb.NewMapObject(&block.Pos, x, y, z, "travelnet")
 	o.Attributes["owner"] = md["owner"]
 	o.Attributes["station_name"] = md["station_name"]
