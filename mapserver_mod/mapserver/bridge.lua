20613101b518d98d11cf7909f3b2e65ea209cba5
NatureFreshMilk <naturefreshmilk@github.com>
2019-01-29 18:00:00 +0100

    player infos in bridge

--- mapserver_mod/mapserver/bridge.lua
+++ mapserver_mod/mapserver/bridge.lua
@@ -1,10 +1,43 @@
 -- mapserver http bridge
 
+
+local function explode(sep, input)
+        local t={}
+        local i=0
+        for k in string.gmatch(input,"([^"..sep.."]+)") do
+            t[i]=k
+            i=i+1
+        end
+        return t
+end
+local function get_max_lag()
+        local arrayoutput = explode(", ",minetest.get_server_status())
+        local arrayoutput = explode("=",arrayoutput[4])
+        return arrayoutput[1]
+end
+
 local http, url, key
 
 function send_stats()
 
-  local data = {}
+  local data = {
+    time = minetest.get_timeofday() * 24000,
+    uptime = minetest.get_server_uptime(),
+    max_lag = get_max_lag(),
+    players = {}
+  }
+
+  for _, player in ipairs(minetest.get_connected_players()) do
+    local info = {
+      name = player:get_player_name(),
+      pos = player:get_pos(),
+      hp = player:get_hp(),
+      breath = player:get_breath(),
+      velocity = player:get_player_velocity()
+    }
+
+    table.insert(data.players, player)
+  end
 
   http.fetch({
     url = url,
