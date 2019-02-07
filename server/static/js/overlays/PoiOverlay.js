fc0e48a95c2439e717f2ba1ce8f0bf19b3664661
Thomas Rudin <thomas@rudin.io>
2019-02-07 20:06:09 +0100

    fix poi overlay

--- server/static/js/overlays/PoiOverlay.js
+++ server/static/js/overlays/PoiOverlay.js
@@ -1,8 +1,19 @@
 'use strict';
 
+var PoiIcon = L.icon({
+  iconUrl:       'css/images/marker-icon.png',
+	shadowUrl:     'css/images/marker-shadow.png',
+	iconSize:    [25, 41],
+	iconAnchor:  [12, 41],
+	popupAnchor: [1, -34],
+	tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+
+
 var PoiOverlay = AbstractIconOverlay.extend({
   initialize: function(wsChannel, layerMgr) {
-    AbstractIconOverlay.prototype.initialize.call(this, wsChannel, layerMgr, "poi", L.Icon.Default);
+    AbstractIconOverlay.prototype.initialize.call(this, wsChannel, layerMgr, "poi", PoiIcon);
   },
 
   createPopup: function(poi){
