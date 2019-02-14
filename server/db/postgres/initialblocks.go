a3bc7fd0eb33e99e54a5de35489f0c2f3266a1bd
NatureFreshMilk <naturefreshmilk@github.com>
2019-02-14 09:13:18 +0100

    comments

--- server/db/postgres/initialblocks.go
+++ server/db/postgres/initialblocks.go
@@ -11,5 +11,15 @@ const (
 )
 
 func (this *PostgresAccessor) FindNextInitialBlocks(s settings.Settings, layers []*layer.Layer, limit int) (*db.InitialBlocksResult, error) {
+
+	//zoom/mapblock-width
+	//13 1
+	//12 2
+	//11 4
+	//10 8
+	//9 16
+
+	//Zoom 9:
+	//10 mapblocks height * 16 * 16 == 2560
 	return nil, nil
 }
