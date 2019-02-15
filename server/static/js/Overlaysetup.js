c1e151a6942197d1ed860f814aaf801edf6cb18e
NatureFreshMilk <naturefreshmilk@github.com>
2019-02-15 12:47:43 +0100

    frontend overlay modules

---
+++ server/static/js/Overlaysetup.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function Overlaysetup(cfg, map, overlays, wsChannel, layerMgr){
+
+    if (cfg.mapobjects.mapserver) {
+      overlays["Player"] = new PlayerOverlay(wsChannel, layerMgr);
+      overlays["POI"] = new PoiOverlay(wsChannel, layerMgr);
+      overlays["Labels"] = new LabelOverlay(wsChannel, layerMgr);
+
+      map.addLayer(overlays["Player"]);
+      map.addLayer(overlays["POI"]);
+      map.addLayer(overlays["Labels"]);
+    }
+
+    if (cfg.mapobjects.travelnet) {
+      overlays["Travelnet"] = new TravelnetOverlay(wsChannel, layerMgr);
+    }
+
+    if (cfg.mapobjects.bones) {
+      overlays["Bones"] = new BonesOverlay(wsChannel, layerMgr);
+    }
+
+    if (cfg.mapobjects.digilines) {
+      overlays["Digilines LCD"] = new LcdOverlay(wsChannel, layerMgr);
+    }
+
+    if (cfg.mapobjects.digiterms) {
+      overlays["Digiterms"] = new DigitermOverlay(wsChannel, layerMgr);
+    }
+
+    if (cfg.mapobjects.luacontroller) {
+      overlays["Lua Controller"] = new LuacontrollerOverlay(wsChannel, layerMgr);
+    }
+
+    if (cfg.mapobjects.technic) {
+      overlays["Technic Anchor"] = new TechnicAnchorOverlay(wsChannel, layerMgr);
+      overlays["Technic Quarry"] = new TechnicQuarryOverlay(wsChannel, layerMgr);
+      overlays["Technic Switching station"] = new TechnicSwitchOverlay(wsChannel, layerMgr);
+    }
+
+    if (cfg.mapobjects.protector) {
+      overlays["Protector"] = new ProtectorOverlay(wsChannel, layerMgr);
+    }
+
+    if (cfg.mapobjects.mission) {
+      overlays["Missions"] = new MissionOverlay(wsChannel, layerMgr);
+    }
+}
