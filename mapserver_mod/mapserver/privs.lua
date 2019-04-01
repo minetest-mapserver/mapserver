7299253fa1a95f955985083535f98f2865afb07a
Thomas Rudin <thomas@rudin.io>
2019-04-01 18:44:50 +0200

    fix irrevokable priv issue

--- mapserver_mod/mapserver/privs.lua
+++ mapserver_mod/mapserver/privs.lua
@@ -1,4 +1,5 @@
 
 minetest.register_privilege("mapserver_hide_player", {
-	description = "Player is hidden from the map"
+	description = "Player is hidden from the map",
+	give_to_singleplayer = false
 })
