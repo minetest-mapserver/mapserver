3d64bdd2c5b0aa045bc92b1dc8e0777e24215e31
Pierre-Yves Rollo <dev@pyrollo.com>
2019-09-25 10:23:36 +0200

    Added zoom level 0 tiles

--- tilerenderer/renderer.go
+++ tilerenderer/renderer.go
@@ -159,6 +159,9 @@ func (tr *TileRenderer) renderImage(tc *coords.TileCoords, recursionDepth int) (
 		buf := new(bytes.Buffer)
 		png.Encode(buf, img)
 
+		tile := buf.Bytes()
+		tr.tdb.SetTile(tc, tile)
+
 		return img, nil
 	}
 
