85a0fc515fbd4a75bd7aa610a8d9ccfd64a9ef66
Pierre-Yves Rollo <dev@pyrollo.com>
2019-09-30 16:24:48 +0200

    Removed unwanted console output

--- mapobject/symbol.go
+++ mapobject/symbol.go
@@ -3,13 +3,11 @@ package mapobject
 import (
 	"mapserver/mapblockparser"
 	"mapserver/mapobjectdb"
-	"fmt"
 )
 
 type SymbolBlock struct{}
 
 func (this *SymbolBlock) onMapObject(x, y, z int, block *mapblockparser.MapBlock) *mapobjectdb.MapObject {
-	fmt.Printf("Symbol at %d %d %d", x, y, z)
 	md := block.Metadata.GetMetadata(x, y, z)
 
 	o := mapobjectdb.NewMapObject(block.Pos, x, y, z, "symbol")
