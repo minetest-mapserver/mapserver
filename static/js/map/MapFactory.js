c73d100559cd72cb185c1cfb800c81406c83039d
Thomas Rudin <thomas@rudin.io>
2019-11-30 17:34:30 +0100

    allow zoom to 12

--- static/js/map/MapFactory.js
+++ static/js/map/MapFactory.js
@@ -16,7 +16,7 @@ export function createMap(node, layerId, zoom, lat, lon){
 
   const map = L.map(node, {
     minZoom: 2,
-    maxZoom: 11,
+    maxZoom: 12,
     center: [lat, lon],
     zoom: zoom,
     crs: SimpleCRS
