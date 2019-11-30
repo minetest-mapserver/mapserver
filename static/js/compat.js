a99e9d48aaf88efa2db2098bf0de4af7dee1f330
Thomas Rudin <thomas@rudin.io>
2019-11-30 17:21:30 +0100

    coord compat

--- static/js/compat.js
+++ static/js/compat.js
@@ -2,14 +2,31 @@
 export function hashCompat(){
 
   if (window.location.hash) {
-    let match = window.location.hash.match(/^#\/(\d*)\/(\d*)\/(\d*)$/m);
-    if (match) {
-      window.location.hash = `#!/map/0/${match[1]}/${match[2]}/${match[3]}`;
+
+    const parts = window.location.hash.replace("#", "").split("/");
+    if (parts.length == 0){
+      //invalid
+      return;
     }
 
-    match = window.location.hash.match(/^#\/(\d*)\/(\d*)\/(\d*)\/(\d*)$/m);
-    if (match) {
-      window.location.hash = `#!/map/${match[1]}/${match[2]}/${match[3]}/${match[4]}`;
+    if (parts[0] == "map"){
+      //new link
+      return;
+    }
+
+    if (isNaN(+parts[0])){
+      //NaN
+      return;
+    }
+
+    if (parts.length == 3){
+      //#1799.5/399/10
+      window.location.hash = `#!/map/0/${parts[0]}/${parts[1]}/${parts[2]}`;
+    }
+
+    if (parts.length == 4) {
+      //#0/-1799.5/399/10
+      window.location.hash = `#!/map/${parts[0]}/${parts[3]}/${parts[1]}/${parts[2]}`;
     }
   }
 }
