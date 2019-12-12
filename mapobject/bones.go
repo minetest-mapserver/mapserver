b777635c6a11396a594130be2c29ac71462664ed
Thomas Rudin <thomas@rudin.io>
2019-12-12 22:11:20 +0100

    check if bones have an inventory

--- mapobject/bones.go
+++ mapobject/bones.go
@@ -11,13 +11,17 @@ type BonesBlock struct{}
 func (this *BonesBlock) onMapObject(x, y, z int, block *mapblockparser.MapBlock) *mapobjectdb.MapObject {
 	md := block.Metadata.GetMetadata(x, y, z)
 
+	invMap := block.Metadata.GetInventoryMapAtPos(x, y, z)
+	mainInv := invMap["main"]
+
+	if mainInv == nil {
+		return nil
+	}
+
 	o := mapobjectdb.NewMapObject(block.Pos, x, y, z, "bones")
 	o.Attributes["time"] = md["time"]
 	o.Attributes["owner"] = md["owner"]
 	o.Attributes["info"] = md["infotext"]
-
-	invMap := block.Metadata.GetInventoryMapAtPos(x, y, z)
-	mainInv := invMap["main"]
 
 	itemCount := 0
 	for _, item := range mainInv.Items {
