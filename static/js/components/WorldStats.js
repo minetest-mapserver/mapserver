e0d10723ecad6586715ba16edb48713054de1f8a
Thomas Rudin <thomas@rudin.io>
2019-12-29 15:43:46 +0100

    externalize worldstats module
    basis for #76

---
+++ static/js/components/WorldStats.js
@@ -0,0 +1,49 @@
+
+export default function(info){
+
+  var timeIcon = m("span", { class: "fa fa-sun", style: "color: orange;" });
+
+  if (info.time < 5500 || info.time > 19000) //0 - 24'000
+    timeIcon = m("span", { class: "fa fa-moon", style: "color: blue;" });
+
+  function getHour(){
+    return Math.floor(info.time/1000);
+  }
+
+  function getMinute(){
+    var min = Math.floor((info.time % 1000) / 1000 * 60);
+    return min > 10 ? min : "0" + min;
+  }
+
+  function getLag(){
+    var color = "green";
+    if (info.max_lag > 0.8)
+      color = "orange";
+    else if (info.max_lag > 1.2)
+      color = "red";
+
+    return [
+      m("span", { class: "fa fa-wifi", style: "color: " + color }),
+      parseInt(info.max_lag*1000),
+      " ms"
+    ];
+  }
+
+  function getPlayers(){
+    return [
+      m("span", { class: "fa fa-users" }),
+      info.players ? info.players.length : "0"
+    ];
+  }
+
+  return m("div", [
+    getPlayers(),
+    " ",
+    getLag(),
+    " ",
+    m("span", { class: "fa fa-clock" }),
+    timeIcon,
+    getHour(), ":", getMinute()
+  ]);
+
+};
