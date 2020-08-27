06e671997bc930c929eb30779993d11892933427
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-27 08:37:37 +0200

    refactor worldstats info

--- static/js/components/WorldStats.js
+++ static/js/components/WorldStats.js
@@ -1,40 +1,25 @@
 import html from "./html.js";
 
 export default function(info){
-
-  var timeIcon = html`<span class="fa fa-sun style="color: orange;"/>`;
-
-  if (info.time < 5500 || info.time > 19000) //0 - 24'000
-    timeIcon = html`<span class="fa fa-moon style="color: blue;"/>`;
-
-  function getHour(){
-    return Math.floor(info.time/1000);
-  }
+  const is_night = info.time < 5500 || info.time > 19000;
 
   function getMinute(){
     var min = Math.floor((info.time % 1000) / 1000 * 60);
     return min >= 10 ? min : "0" + min;
   }
 
-  function getLag(){
-    var color = "green";
-    if (info.max_lag > 0.8)
-      color = "orange";
-    else if (info.max_lag > 1.2)
-      color = "red";
-
-    return html`<span class="fa fa-wifi" style="color: ${color}"/> ${parseInt(info.max_lag*1000)} ms`;
-  }
-
-  function getPlayers(){
-    return html`<span class="fa fa-users"/> ${info.players ? info.players.length : "0"}`;
-  }
+  var color = "green";
+  if (info.max_lag > 0.8)
+    color = "orange";
+  else if (info.max_lag > 1.2)
+    color = "red";
 
   return html`<div>
-    ${getPlayers()}
-    ${getLag()}
-    <span class="fa fa-clock"></span>
-    ${timeIcon}
-    ${getHour()}:${getMinute()}
+    <span class="fa fa-users"/> ${info.players ? info.players.length : "0"}
+    <span class="fa fa-wifi" style="color: ${color}"/>
+    ${parseInt(info.max_lag*1000)} ms
+    <span class="fa fa-clock"/>
+    <span class="fa fa-${is_night ? "moon" : "sun"}" style="color: ${is_night ? "blue" : "orange"};"/>
+    ${Math.floor(info.time/1000)}:${getMinute()}
   </div>`;
 }
