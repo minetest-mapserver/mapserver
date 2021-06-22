e4f57a2170a4d97a854e3645823e64ad5959dcc9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-06-22 19:43:19 +0200

    frontend cleanup

--- public/embed.go
+++ public/embed.go
@@ -2,12 +2,11 @@ package public
 
 import "embed"
 
-//go:embed colors/*
-//go:embed css/*
-//go:embed pics/*
-//go:embed sql/*
-//go:embed webfonts/*
-//go:embed *.html
-//go:embed *.txt
-//go:embed js/*
+//go:embed colors/* css/* pics/* sql/* webfonts/* *.html *.txt js/*
+//go:embed node_modules/bootstrap/dist/css/bootstrap.min.css
+//go:embed node_modules/vue/dist/vue.min.js
+//go:embed node_modules/vue-router/dist/vue-router.min.js
+//go:embed node_modules/vue-i18n/dist/vue-i18n.min.js
+//go:embed node_modules/@fortawesome/fontawesome-free/css/all.min.css
+//go:embed node_modules/@fortawesome/fontawesome-free/webfonts/*
 var Files embed.FS
