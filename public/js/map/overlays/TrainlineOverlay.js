f9382cb2b5469b6649f4618a91557887ce49a770
Peter Nerlich <peter.nerlich+dev@googlemail.com>
2022-01-13 11:10:12 +0100

    more optimizations

--- public/js/map/overlays/TrainlineOverlay.js
+++ public/js/map/overlays/TrainlineOverlay.js
@@ -4,56 +4,13 @@ import { getMapObjects } from '../../api.js';
 export default AbstractGeoJsonOverlay.extend({
   initialize: function() {
     AbstractGeoJsonOverlay.prototype.initialize.call(this, "train");
-    this.cache = {}; // { "A1":[] }
+    this.cache = {
+      lines: {}, // { "A1":[] }
+      lineColors: {}, // { "A1": "red" }
+      lineFeat: []
+    };
     this.pendingQueries = [];
-    this.lastLayer = L.geoJSON([], {}); // empty dummy, will be replaced later
-  },
-  
-  createGeoJson: function(objects){
-    var self = this;
-    // which unique lines do objects belong to?
-    var lines = [];
-    objects.forEach(function(obj){
-      if (obj.attributes.line && lines.indexOf(obj.attributes.line) == -1) {
-        lines.push(obj.attributes.line);
-      }
-    });
-    // query for each line, add to cache
-    lines.forEach(function(l){
-      if (!self.cache[l]){
-        // only request if not in cache.
-        // if someone changed the train lines, the user has to reload. sorry.
-        self.pendingQueries.push(l);
-        getMapObjects({
-          type: self.type,
-          attributelike: {
-            key: "line",
-            value: l
-          }
-        })
-        .then(function(objects){
-          self.cache[l] = objects;
-          self.pendingQueries = self.pendingQueries.filter(function(e){
-            return e != l;
-          });
-          if (self.pendingQueries.length == 0) {
-            // trigger a redraw
-            // this will only happen if anything changed since it runs only after we completed a query
-            self.clearLayers();
-
-            var geoJsonLayer = self.createGeoJsonInternal(self.cache);
-            geoJsonLayer.addTo(self);
-          }
-        });
-      }
-    });
-    
-    return self.lastLayer;
-  },
-
-  createGeoJsonInternal: function(lines){
-
-    var geoJsonLayer = L.geoJSON([], {
+    this.lastLayer = L.geoJSON([], {
       onEachFeature: function(feature, layer){
         if (feature.properties && feature.properties.popupContent) {
           layer.bindPopup(feature.properties.popupContent);
@@ -75,80 +32,101 @@ export default AbstractGeoJsonOverlay.extend({
         }
       }
     });
+  },
+  
+  createGeoJson: function(objects){
+    var self = this;
 
-    var lineColors = {}; // { "A1": "red" }
-
-    // already sorted, determine color
-    Object.keys(lines).forEach(function(linename){
-      if (!lineColors[linename]){
-        lineColors[linename] = "#ff7800";
-        for (var i = lines[linename].length-1; i >= 0; i--) {
-          // find the last element specifying a color
-          // as was previous behaviour, but be more efficient
-          if (lines[linename][i].attributes.color){
-            lineColors[linename] = lines[linename][i].attributes.color;
-            break;
-          }
-        }
+    // which unique lines do objects belong to?
+    var lines = [];
+    objects.forEach(function(obj){
+      if (obj.attributes.line && lines.indexOf(obj.attributes.line) == -1) {
+        lines.push(obj.attributes.line);
       }
     });
 
-    //Order by index and display
-    Object.keys(lines).forEach(function(linename){
-      lines[linename].sort(function(a,b){
-        return parseInt(a.attributes.index) - parseInt(b.attributes.index);
-      });
+    // query for each line, add to cache
+    lines.forEach(function(linename){
+      if (!self.cache.lines[linename]){
+        // only request if not in cache.
+        // if someone changed the train lines, the user has to reload. sorry.
+        self.pendingQueries.push(linename);
+        getMapObjects({
+          type: self.type,
+          attributelike: {
+            key: "line",
+            value: linename
+          }
+        })
+        .then(function(objects){
+          objects.sort(function(a,b){
+            return parseInt(a.attributes.index) - parseInt(b.attributes.index);
+          });
 
-      var coords = [];
-      var stations = [];
+          self.cache.lines[linename] = objects;
+          // already sorted, determine color
+          self.cache.lineColors[linename] = "#ff7800";
+          for (var i = objects.length-1; i >= 0; i--) {
+            // find the last element specifying a color
+            // as was previous behaviour, but be more efficient
+            if (objects[i].attributes.color){
+              self.cache.lineColors[linename] = objects[i].attributes.color;
+              break;
+            }
+          }
 
-      //Add stations
-      lines[linename].forEach(function(entry){
-        coords.push([entry.x, entry.z]);
+          var feat = {
+            coords: [],
+            stations: [],
+            feature: null
+          };
+          //Add stations
+          objects.forEach(function(entry){
+            feat.coords.push([entry.x, entry.z]);
 
-        if (entry.attributes.station) {
-          stations.push({
-            "type": "Feature",
-            "properties": {
-              "name": entry.attributes.station,
-              "color": lineColors[linename],
-              "popupContent": "<b>Train-station (Line " + entry.attributes.line + ")</b><hr>" +
-                entry.attributes.station
-            },
-            "geometry": {
-              "type": "Point",
-              "coordinates": [entry.x, entry.z]
+            if (entry.attributes.station) {
+              feat.stations.push({
+                "type": "Feature",
+                "properties": {
+                  "name": entry.attributes.station,
+                  "color": self.cache.lineColors[linename],
+                  "popupContent": "<b>Train-station (Line " + entry.attributes.line + ")</b><hr>" +
+                    entry.attributes.station
+                },
+                "geometry": {
+                  "type": "Point",
+                  "coordinates": [entry.x, entry.z]
+                }
+              });
             }
           });
-        }
-      });
 
-      var feature = {
-        "type":"Feature",
-        "geometry": {
-          "type":"LineString",
-          "coordinates":coords
-        },
-        "properties":{
-            "name": linename,
-            "color": lineColors[linename],
-            "popupContent": "<b>Train-line (" + linename + ")</b>"
-        }
-      };
+          feat.feature = {
+            "type":"Feature",
+            "geometry": {
+              "type":"LineString",
+              "coordinates": feat.coords
+            },
+            "properties":{
+                "name": linename,
+                "color": self.cache.lineColors[linename],
+                "popupContent": "<b>Train-line (" + linename + ")</b>"
+            }
+          };
 
-      //line-points
-      geoJsonLayer.addData(feature);
+          self.cache.lineFeat[linename] = feat;
 
-      //stations
-      stations.forEach(function(stationfeature){
-        geoJsonLayer.addData(stationfeature);
-      });
+          //line-points
+          self.lastLayer.addData(feat.feature);
 
-
+          //stations
+          feat.stations.forEach(function(stationfeature){
+            self.lastLayer.addData(stationfeature);
+          });
+        });
+      }
     });
 
-    this.lastLayer = geoJsonLayer;
-    return geoJsonLayer;
-  }
-
+    return self.lastLayer;
+  },
 });
