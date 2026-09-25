2ea20aaa2cdee386c95d67d6bf333cad1944054c
B. Stack <bgstack15@gmail.com>
2026-09-25 12:52:12 -0400

    respawnplaces: add color and icon

--- respawnparser/respawnparser.go
+++ respawnparser/respawnparser.go
@@ -4,9 +4,10 @@ package respawnparser
 // docker go cache help: https://oneuptime.com/blog/post/2026-02-08-how-to-speed-up-docker-build-for-go-projects/view
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 type File map[string]RespawnPlace
@@ -15,6 +16,8 @@ type RespawnPlace struct {
 	FullName string     `json:"full_name,omitempty"`
 	Pos      GenericPos `json:"pos"`
 	Look     *Direction `json:"look,omitempty"`
+	Color    string     `json:"color,omitempty"`
+	Icon     string     `json:"icon,omitempty"`
 }
 
 type GenericPos struct {
@@ -29,31 +32,52 @@ type Direction struct {
 }
 
 func getInt(o interface{}) int {
-    v, _ := o.(float64)
-    return int(v)
+	v, _ := o.(float64)
+	return int(v)
 }
 
 func (pos *GenericPos) UnmarshalJSON(data []byte) error {
 	m := make(map[string]interface{})
+
 	err := json.Unmarshal(data, &m)
 	if err != nil {
 		return err
 	}
-    pos.X = getInt(m["x"])
-    pos.Y = getInt(m["y"])
-    pos.Z = getInt(m["z"])
-	// do not cast to int like https://github.com/minetest-go/areasparser/blob/master/parser.go
+
+	pos.X = getInt(m["x"])
+	pos.Y = getInt(m["y"])
+	pos.Z = getInt(m["z"])
+
 	return nil
 }
 
+// Parse only the contents before the first newline, because
+// mods/respawn/storage.lua save_db() uses newline as EOF indicator.
+func Parse(data []byte) (File, error) {
+	if newline := bytes.IndexByte(data, '\n'); newline >= 0 {
+		data = data[:newline]
+	}
+
+	// Remove possible windows-style CR line ending
+	data = bytes.TrimSuffix(data, []byte{'\r'})
+
+	var respawnplaces File
+	if err := json.Unmarshal(data, &respawnplaces); err != nil {
+		return nil, fmt.Errorf("parse respawn data: %w", err)
+	}
+
+	return respawnplaces, nil
+}
+
+// ParseFile reads a file and passes its contents to Parse.
 func ParseFile(filename string) (File, error) {
-	data, err := ioutil.ReadFile(filename)
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
 
-	var respawnplaces File
-	if err := json.Unmarshal(data, &respawnplaces); err != nil {
+	respawnplaces, err := Parse(data)
+	if err != nil {
 		return nil, fmt.Errorf("parse %s: %w", filename, err)
 	}
 
